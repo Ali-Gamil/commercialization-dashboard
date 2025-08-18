f9bd6936824dba6f9212c9e12d30a3f534d9f5b2
Ali-Gamil <aligamil8524@gmail.com>
2025-08-18 18:19:29 +0300

    Update Questionnaire.py

--- Questionnaire.py
+++ Questionnaire.py
@@ -3,7 +3,7 @@ import pandas as pd
 import io
 
 st.set_page_config(layout="wide")
-st.title("📝 Company Commercialization Scoring Dashboard")
+st.title("✔/❌ Prelimenary Sorting Dashboard")
 
 # --- Questions ---
 questions = [
