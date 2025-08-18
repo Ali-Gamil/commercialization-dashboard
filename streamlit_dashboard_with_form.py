80aaceb179bd55c8ee8fc9640cfc0d67135eb95d
Ali-Gamil <aligamil8524@gmail.com>
2025-08-18 18:18:32 +0300

    Update streamlit_dashboard_with_form.py

--- streamlit_dashboard_with_form.py
+++ streamlit_dashboard_with_form.py
@@ -3,7 +3,7 @@ import pandas as pd
 import io
 
 st.set_page_config(layout="wide")
-st.title("📝 Company Commercialization Scoring Dashboard")
+st.title("✔/❌ Prelimenary Sorting Dashboard")
 
 criteria_info = {
     "Business Model": "How well the company generates and sustains revenue",
